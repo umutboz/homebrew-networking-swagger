25a34d1239f70acbe25fe8807569b91dad45beaa
02483564 <umut.boz@kocsistem.com.tr>
2019-01-17 05:46:16 +0300

    as2

--- networking-swagger.rb
+++ networking-swagger.rb
@@ -1,9 +1,9 @@
 class Viperize < Formula
     desc "Networking-swagger java code generator 1.0"
     homepage ""
-    url "https://github.com/umutboz/homebrew-networking-swagger/archive/1.0.tar.gz"
+    url "https://github.com/umutboz/networking-swagger/archive/1.0.tar.gz"
     version "1.0"
-    sha256 "8a859e536a76a0fde08d19afffe6cc670cdef97914fcfac79602ccd6d2102316"
+    sha256 "31dde5d1e91c74edb61e466554bf6fe98bfb9d0cfb6ed0f7950f99d89465ffd2"
 
     # depends_on "cmake" => :build
 
