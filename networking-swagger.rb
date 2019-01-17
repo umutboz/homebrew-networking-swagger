2bb84d17c8eca697f0449965d0e89f713f2ea45d
02483564 <umut.boz@kocsistem.com.tr>
2019-01-17 06:23:01 +0300

    as2

--- networking-swagger.rb
+++ networking-swagger.rb
@@ -1,4 +1,4 @@
-class Networking-Swagger < Formula
+class NetworkingSwagger < Formula
     desc "Networking-swagger java code generator 1.0"
     homepage ""
     url "https://github.com/umutboz/networking-swagger/archive/1.0.tar.gz"
