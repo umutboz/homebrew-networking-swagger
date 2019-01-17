89a5fa02c274cbeca265576e25dabd17e13b2ffc
02483564 <umut.boz@kocsistem.com.tr>
2019-01-17 05:03:49 +0300

    frst2 .

---
+++ networking-swagger.rb
@@ -0,0 +1,13 @@
+class Viperize < Formula
+    desc "Networking-swagger java code generator 1.0"
+    homepage ""
+    url "https://github.com/umutboz/networking-swagger/archive/1.0.tar.gz"
+    sha256 "8a859e536a76a0fde08d19afffe6cc670cdef97914fcfac79602ccd6d2102316"
+    # depends_on "cmake" => :build
+
+    def install
+        libexec.install "networking-swagger-java.sh"
+        bin.install_symlink libexec/"networking-swagger-java.sh" => "networking-swagger-java"
+    end
+
+end
